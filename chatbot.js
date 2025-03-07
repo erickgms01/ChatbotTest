9972d472f87bb7d5a03dfde110576a1d61bd89a3
erickgms01 <erickderi98@gmail.com>
2025-03-07 11:32:59 -0300

    k

--- chatbot.js
+++ chatbot.js
@@ -1,7 +1,12 @@
 // leitor de qr code
 const qrcode = require('qrcode-terminal');
 const { Client, Buttons, List, MessageMedia } = require('whatsapp-web.js'); // Mudança Buttons
-const client = new Client();
+const client = new Client({
+    puppeteer: {
+        headless: true, // Executar sem interface gráfica (obrigatório no Railway)
+        args: ['--no-sandbox', '--disable-setuid-sandbox'] // Evita erros de permissão
+    }
+});
 // serviço de leitura do qr code
 client.on('qr', qr => {
     qrcode.generate(qr, {small: true});
